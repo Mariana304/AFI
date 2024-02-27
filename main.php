225c09a95744dcc6fd9aadce62bfc5324fa66763
Mariana304 <marianaortegaortiz92@gmail.com>
2024-02-27 11:48:37 -0500

    maquetacion de diseño

--- main.php
+++ main.php
@@ -1,6 +1,8 @@
 
 <?php
 
+echo '<link rel="stylesheet" href="/CSS/style.css">';
+echo '<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />';
 
 $groups = ["202401_MD_AFIV04_1", "202401_MD_AFIV04_10", "Grupo 1"]; // array ["202401_MD_AFIV04_1","202401_MD_AFIV04_10","Grupo 1"]
 $fileCode = 1; // string (valor que se busca en el CSV), ejemplo "ACC-1"
@@ -12,26 +14,25 @@ $role = "student"; // string con tres valores posibles  "manager" "teacher" "stu
 $rutaArchivoCSV = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRdl97qZGba8l61ifQ7Wv9mMPhUzU6ENeTFmRPE3IOzaKSe9tKyD_Dkf4vQcFfNnCTM02f8kyioGjj9/pub?gid=0&single=true&output=csv"; // Ruta del archivo CSV
 
 
-//Primero se pregunta si hay grupos ya que si no hay no se podra hacer la busqueda
+//Primero se pregunta si hay grupos ya que si no hay no se podra hacer la busqueda (en los datos recibidos)
 
-if (empty($groups)) { 
-    echo "No hay grupos ";
+
+
+if (empty($groups)) {
+
+    echo "<div id='msg-not-groups'>No hay grupos en los datos recibidos</div>";
 } else {
-
 
     //Se lee el archivo CSV
     $archivoCSV = fopen($rutaArchivoCSV, "r");
-
 
     //Se almacena en un array 
     $data = array();
 
 
-
     //se genera un ciclo en donde en la variable $fila se extrae lo que se tomo del archivo
     while (($fila = fgetcsv($archivoCSV)) !== FALSE) {
         $data[$fila[0]] = $fila;
-
     }
 
     fclose($archivoCSV);
@@ -44,53 +45,62 @@ if (empty($groups)) {
     foreach ($groups as $group) {
 
 
-        //se pregunta si hay contenido en los grupos CSV
+
+        
+        //si hay grupos en los datos recibidos 
         if (isset($data[$group])) {
 
+            $teacher = $data[$group][2];
 
             //guarda en la variable $enlace el indice 3 de el array ya que ahi esta el enlace, y se le quita todos los espacios para poder ser verificado 
             $enlace = trim($data[$group][3]);
 
+            if (empty($enlace)) { //se pregunta si no hay enlace 
 
-            //se pregunta si hay un enlace en el CSV, y si el enlace que hay es valido 
-            if (isset($enlace) && filter_var($enlace, FILTER_VALIDATE_URL)) {
-                echo "El enlace es válido y si hay un enlace el el archivo ";
-                echo "<br>";
-                echo "Enlace: " . $enlace;
-                echo "<br> <br>";
 
-                // Mostrar la información del grupo
-                echo "<div style='...'>";
-                echo "<div style='...'>";
-                echo "Aquí esta tu actividad de consolidación de conocimiento para el grupo $group";
-                echo "</div>";
-                echo "<br>";
-                echo "<div style='...'>";
-                echo "<a style='...' href='$enlace' target='_blank'>Actividad de consolidacion</a>";
-                echo "</div>";
-                echo "</div>";
-                echo ".<br><br>";
-                echo "<hr/>";
+                echo "<div id='msg-not-link'>No hay enlace para la ACC del Grupo: $group </div><br><br><hr/>";
+            } else { // si hay enlace ...
+
+                //se pregunta si el enlace es valido
+                if (filter_var($enlace, FILTER_VALIDATE_URL)) {
+
+                    // Mostrar la información del grupo
+
+                    echo "<div id='activity'> Aquí esta tu actividad de consolidación número $fileCode, propuesta por el profesor $teacher, para el grupo $group <br><br><br>";
+
+                    echo "<span id='icon-Arrow' class='material-symbols-outlined'>
+                    arrow_downward
+                    </span> <br><br>";
+
+                    echo "<a id='button-acc' style='' href='$enlace' target='_blank'>Actividad de consolidación</a></div><br><br><hr/>";
+                } else { //si el enlace no es valido...
+
+                    echo "<div id='msg-invalid-link'>El enlace no es valido para la ACC del Grupo: . $group </div><br><br><hr/>";
+                }
             }
+        } else { //se busca grupo para todos los datos recibidos en el array de grupos, y si no hay grupos...
 
 
-            //en caso de que no haya enlace o no sea valido el enlace
-            else {
-
-                echo "No es un enlace válido o no hay enlace ";
-                echo "<br>";
-                echo " Enlace: " . $enlace;
-                echo ".<br><br>";
-                echo "<hr/>";
-            }
-        } else {
-
-            echo "no hay grupo";
-            echo "<div style='color: red; font-weight: bold;'>";
-            echo "Error: El grupo '$group' no se encuentra.";
-            echo "</div>";
+            echo "<div id='msg-indefinite-group' style=''>Error: El grupo '$group' no se encuentra.</div><br><br><hr/>";
         }
-
-
     }
 }
+
+
+echo "<script src='https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js'></script>";
+
+
+// echo "<div  class='loader' style='position: fixed;left: 0px;top: 0px;width: 100%;height: 100%;z-index: 9999;background: url('../img/merry.gif') 50% 50% no-repeat #bd302d;background-size: 100%;'></div>";
+
+echo " <div class='loader' style='position: fixed;left: 0px;top: 0px;width: 100%;height: 100%;z-index: 9999;background:#bd302d; background-size: 100%;'><img src='/images/loading.gif'></div>";
+
+
+
+
+echo "<script>
+
+$(window).load(function () {
+    $('.loader').fadeOut('slow');
+});
+
+</script>";
